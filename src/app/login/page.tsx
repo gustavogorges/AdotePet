55b146a2aacb120d682601a183c990ed50b3384e
gustavogorges <gustavo_gorges@hotmail.com>
2024-05-30 00:29:57 -0300

    feat : finish the register page design, from now create the model of User and test the api with it to later move on, on the next pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,27 @@
 'use client'
 import { useRouter } from "next/navigation";
-import Nav from "../../components/nav/nav"
+import Nav from "../../components/nav/nav";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { blue, grey } from "@mui/material/colors";
 
 
-const Login =() => {
+const Login = () => {
     const router = useRouter();
-  return (
-    <div className=" w-full overflow-y-auto h-screen bg-[#eeeeee] flex flex-col items-center">
-      <Nav></Nav>
-      <div className=" w-[70%] h-full flex flex-col justify-center items-center">
-        <div className="w-full h-[8%] flex items-center justify-start">
-          <div className=" h-full w-16 bg-pink-300 rounded-full cursor-pointer">
+    return (
+        <div className=" w-full overflow-y-auto h-screen bg-[#eeeeee] flex flex-col items-center">
+            <Nav></Nav>
+            <div className=" w-[70%] h-full flex flex-col  items-center">
+                <div className="w-full h-[10%] flex items-center justify-start">
+                    <div className=" absolute h-16 w-16 bg-[#3399BB] flex justify-center items-center rounded-full cursor-pointer">
+                        <ArrowBackIcon sx={{ color: grey[50] }} fontSize="large" />
+                    </div>
+                </div>
+                <div className="w-[40%] h-[70%] rounded-xl border-2 bg-white">
 
-          </div>
+                </div>
+            </div>
         </div>
-        <div className="w-[40%] h-[70%] rounded-xl border-2 bg-white">
-
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -12,10 +12,10 @@ const Nav = () => {
     ]
 
     return(
-        <div className=" w-full h-[12%] bg-white flex justify-center items-center">
+        <div className=" w-full h-[12%] bg-white flex justify-center items-center mb-12">
             <div className="w-[70%] h-full bg-white grid grid-cols-4">
-                <div className="col-span-1 w-[50%] flex justify-start items-center bg-pink-300">
-                    {/*<Image src="/public/logo.png" alt="" width={100} height={100} />*/}
+                <div className="col-span-1 w-[50%] flex justify-start items-center cursor-pointer">
+                    <Image src='/logo.png' alt="logo" width={150} height={100}/>
                 </div>
                 <div className=" col-start-2 col-span-2 flex items-center justify-center gap-8">
                     {
@@ -31,8 +31,8 @@ const Nav = () => {
                     }
                 </div>
                 <div className=" col-start-4 col-span-1 h-full flex items-center justify-end gap-8">
-                    <p onClick={() => router.push('/login')} className=" text-pink-400 text-lg font-bold cursor-pointer">Entrar</p>
-                    <p onClick={() => router.push('/register')} className=" text-pink-400 text-lg font-bold cursor-pointer">Cadastre-se</p>
+                    <p onClick={() => router.push('/login')} className="underline text-pink-400 text-lg font-bold cursor-pointer">Entrar</p>
+                    <p onClick={() => router.push('/register')} className=" text-white text-lg py-2 px-2 rounded-xl bg-[#D971A1] font-bold cursor-pointer">Cadastre-se</p>
                 </div>
             </div>
         </div>
